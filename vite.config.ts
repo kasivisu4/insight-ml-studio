4e877cf14ea6c7760aae1963964c63f140c7fa15
kasi viswanath Vandanapu <kasivisu4@gmail.com>
2025-04-27 22:49:57 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/insight-ml-studio/"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
