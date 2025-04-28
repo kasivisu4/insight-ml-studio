c9a1468cccd324e7ae56769b3061cf2e46622d8b
kasivisu4 <kasivisu4@gmail.com>
2025-04-28 00:39:56 -0400

    updated the base

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
